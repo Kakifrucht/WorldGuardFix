1e8b50aa0500f1bf06c7e456cef3826b295693a0
Kakifrucht <Kakifrucht@users.noreply.github.com>
2016-05-03 17:09:53 +0200

    Fix blacklisted items potion use, closes #4, implement chorus fruit protection, closes #6

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # WorldGuardFix
-Bukkit plugin that adds some missing protections for Minecraft/WorldGuard 1.9
+Bukkit plugin that adds some missing protections for Minecraft/WorldGuard 1.9.
+Download link can be found on [SpigotMC](https://www.spigotmc.org/resources/worldguard-fix.22712/).
 
-You can find a download link on [SpigotMC](https://www.spigotmc.org/resources/worldguard-fix.22712/).
 This plugin is dependant on [WorldGuard](https://github.com/sk89q/WorldGuard). It is drop and go and does not need any configuration.
 
 ## Current features
 
 - Hooks into WorldGuard
 - Disables fishing hook in no PvP regions
+- Fixes potion blacklist, potions can no longer be used
 - Disables lingering potions in no splash regions
   - Disables areacloud effect in no splash regions aswell
 - Disable frost walker in no build regions
 - Disable placement of boats in no build regions
 - Disable placement of endercrystals in no build regions
-- Protect lilypad destroy with boats in regions (will remove boat)
+- Disable chrous fruit tp in protected regions
+- Protect lilypads from being destroyed with boats in regions (will remove boat)
--- src/de/halfminer/worldguardfix/Listeners.java
+++ src/de/halfminer/worldguardfix/Listeners.java
@@ -1,24 +1,29 @@
 package de.halfminer.worldguardfix;
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.bukkit.cause.Cause;
+import com.sk89q.worldguard.bukkit.event.inventory.UseItemEvent;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
 import net.md_5.bungee.api.ChatColor;
 import net.minecraft.server.v1_9_R1.EntityFishingHook;
 import net.minecraft.server.v1_9_R1.EntityPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
 import org.bukkit.entity.*;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.EntityBlockFormEvent;
-import org.bukkit.event.entity.AreaEffectCloudApplyEvent;
-import org.bukkit.event.entity.EntityChangeBlockEvent;
-import org.bukkit.event.entity.LingeringPotionSplashEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.*;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.Iterator;
+import java.util.Set;
 
 class Listeners implements Listener {
 
@@ -60,6 +65,11 @@ class Listeners implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void disableLingeringPotions(LingeringPotionSplashEvent e) {
         if (!helper.isAllowed(e.getEntity().getLocation(), DefaultFlag.POTION_SPLASH)) e.setCancelled(true);
+
+        // call UseItemEvent
+        Bukkit.getServer().getPluginManager().callEvent(new UseItemEvent(e,
+                Cause.create((Player) e.getEntity().getShooter()),
+                e.getEntity().getLocation().getWorld(), e.getEntity().getItem()));
     }
 
     @EventHandler
@@ -85,14 +95,14 @@ class Listeners implements Listener {
                     || e.getItem().getType().toString().startsWith("BOAT")) {
                 e.setCancelled(true);
                 e.getPlayer().sendMessage(ChatColor.RED.toString() + ChatColor.BOLD
-                        + "Hey! " + ChatColor.GRAY + "Sorry, but you can't place that block here.");
+                        + "Hey! " + ChatColor.GRAY + "Sorry, but you can't put that here.");
                 e.getPlayer().updateInventory();
             }
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST,ignoreCancelled = true)
-    public void disableBoatLilypadBreak(final EntityChangeBlockEvent e) {
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void disableBoatLilypadBreak(EntityChangeBlockEvent e) {
 
         if (e.getBlock().getType().equals(Material.WATER_LILY) && helper.hasRegion(e.getBlock().getLocation())) {
             e.setCancelled(true);
@@ -101,6 +111,45 @@ class Listeners implements Listener {
             // force block update
             e.getBlock().setType(Material.AIR);
             e.getBlock().setType(Material.WATER_LILY);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void disableChorusFruitTp(PlayerTeleportEvent e) {
+
+        //TODO use custom flag
+        if (e.getCause().equals(PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT)) {
+            if (helper.hasRegion(e.getTo())) e.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void disableBlacklistedPotionUse(UseItemEvent e) {
+
+        Material material = e.getItemStack().getType();
+        if (material.equals(Material.SPLASH_POTION)
+                || material.equals(Material.POTION)
+                || material.equals(Material.LINGERING_POTION)) {
+
+            Player p = e.getCause().getFirstPlayer();
+
+            if (p != null && helper.getWorldGuard().hasPermission(p, "worldguard.override.potions")) return;
+
+            Set<PotionEffectType> set = helper.getWorldGuard().getGlobalStateManager().get(e.getWorld()).blockPotions;
+            PotionEffectType type = ((PotionMeta) e.getItemStack().getItemMeta()).getBasePotionData().getType().getEffectType();
+            if (set.contains(type)) {
+
+                if (e.getOriginalEvent() != null && e.getOriginalEvent() instanceof Cancellable) {
+                    ((Cancellable) e.getOriginalEvent()).setCancelled(true);
+                }
+                e.setCancelled(true);
+                if (p != null) {
+                    p.updateInventory();
+                    p.sendMessage(ChatColor.RED + "Sorry, potions with " + type.getName() + " can't be used.");
+
+                }
+                e.getCause().getFirstPlayer().updateInventory();
+            }
         }
     }
 }
